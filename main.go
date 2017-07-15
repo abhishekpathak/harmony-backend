a57fab0eaee3f17715552d9f86229158d0789a0a
Abhishek Pathak <me@abhishekpathak.net>
2017-07-15 13:26:44 +0530

    Add Heroku-related dependencies

--- main.go
+++ main.go
@@ -27,17 +27,8 @@ func CheckError(err error) {
 	}
 }
 
-const (
-  host     = "localhost"
-  port     = 5432
-  user     = "harmony"
-  password = "harmony"
-  dbname   = "harmony"
-)
-
 func GetDbHandle() *sql.DB {
-	DSN := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", DSN)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		panic(err)
 	}
